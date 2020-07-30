ef8beddb6b9c7ab438603acb483d48e7a232c38a
Taeuk Kang <rkdxodnr07@gmail.com>
2020-07-30 10:55:46 +0900

    Update test3_exception.py

--- day12/test3_exception.py
+++ day12/test3_exception.py
@@ -11,6 +11,8 @@ except ????:
     예외처리문장2
 except ????:
     예외처리문장3
+finally:
+    예외가 있던 없던 반드시 실행되는 문장
 
 '''
 
@@ -18,7 +20,10 @@ import random
 
 # 사용자 정의 에러 raise 로 발생시킨다
 class EvenError(Exception):
-    pass
+    def __init__(self, msg):
+        self.msg = msg
+    def __str__(self):
+        return self.msg
 
 
 # 0~9 사이의 정수를 랜덤하게 생성
@@ -26,13 +31,14 @@ try:
     n = int(input("숫자입력 : "))
     # 사용자가 입력한 값이 짝수이면 실행안함
     if n % 2 == 0:
-        raise EvenError
+        raise EvenError("홀수만 입력해라 ㅡㅡ^")
 
     for i in range(10):
         a = random.randint(0,9)
         print(n/a)
 
-except EvenError:
+except EvenError as ee:
+    print(ee)
     print("짝수는 계산 안해줘..")
 
 except ZeroDivisionError:
@@ -40,3 +46,6 @@ except ZeroDivisionError:
 
 except ValueError:
     print("숫자만 입력하세요")
+
+finally : 
+    print("이 부분은 예외가 있던 없던 항상 실행됩니다.")
