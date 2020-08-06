d8d46bb92f27658c2f4c4b9f46ed3dd65e1e6219
Taeuk Kang <rkdxodnr07@gmail.com>
2020-08-06 18:02:24 +0900

    그림판

--- day17/paint.py
+++ day17/paint.py
@@ -6,10 +6,12 @@ from PyQt5.QtWidgets import *
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
+        # 1.직선 2.사각형
+        self.drawType = 1
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(30,30,1000,1000)
+        self.setGeometry(30,30,800,800)
         # 전체 폼
         frmbox = QHBoxLayout()
         self.setLayout(frmbox)
@@ -25,10 +27,23 @@ class MyApp(QWidget):
         box1 = QVBoxLayout()
         gb.setLayout(box1)
 
-        box1.addWidget(QRadioButton("직선", self))
-        box1.addWidget(QRadioButton("곡선", self))
-        box1.addWidget(QRadioButton("사각형", self))
-        box1.addWidget(QRadioButton("타원", self))
+        self.rbtnLine = QRadioButton("직선", self)
+        self.rbtnRect = QRadioButton("사각형", self)
+        self.rbtnCurve = QRadioButton("곡선", self)
+        self.rbtnEllipse =QRadioButton("타원", self)
+        box1.addWidget(self.rbtnLine)
+        box1.addWidget(self.rbtnRect)
+        box1.addWidget(self.rbtnCurve)
+        box1.addWidget(self.rbtnEllipse)
+        self.rbtnLine.clicked.connect(self.radioBtnClicked)
+        self.rbtnRect.clicked.connect(self.radioBtnClicked)
+        self.rbtnCurve.clicked.connect(self.radioBtnClicked)
+        self.rbtnEllipse.clicked.connect(self.radioBtnClicked)
+
+        self.rbtnLine.setChecked(True)
+        self.drawType = 1
+
+        
 
         # 그룹박스 2
         gb2 = QGroupBox("Pen setting")
@@ -40,10 +55,10 @@ class MyApp(QWidget):
         label = QLabel("선 굵기")
         grid.addWidget(label, 0, 0)
 
-        combo = QComboBox()
-        grid.addWidget(combo, 0, 1)
+        self.combo = QComboBox()
+        grid.addWidget(self.combo, 0, 1)
         for i in range(1,21):
-            combo.addItem(str(i))
+            self.combo.addItem(str(i))
 
         label2 = QLabel("선 색")
         grid.addWidget(label2, 1, 0)
@@ -52,6 +67,7 @@ class MyApp(QWidget):
         self.penbtn = QPushButton()
         self.penbtn.setStyleSheet("background-color: rgb(0,0,0)")
         grid.addWidget(self.penbtn,1,1)
+        self.penbtn.clicked.connect(self.selectColorDlg)
 
         # 그룹박스 3
         # 붓 설정
@@ -64,10 +80,21 @@ class MyApp(QWidget):
         label3 = QLabel("붓 색상")
         hbox.addWidget(label3)
 
+        gb4 = QGroupBox("FILE")
+        leftbox.addWidget(gb4)
+
+        hbox4 = QHBoxLayout()
+        gb4.setLayout(hbox4)
+
+        saveBtn = QPushButton("버튼", self)
+        hbox4.addWidget(saveBtn)
+        saveBtn.clicked.connect(self.save_img)
+
         self.brushcolor = QColor(255,255,255)
         self.brushbtn = QPushButton()
         self.brushbtn.setStyleSheet("background-color:rgb(255,255,255);")
         hbox.addWidget(self.brushbtn)
+        self.brushbtn.clicked.connect(self.selectColorDlg)
 
         # 우측 레이아웃 박스에 그래픽 뷰 추가
         self.view = CGView(self)
@@ -78,6 +105,40 @@ class MyApp(QWidget):
         frmbox.addLayout(leftbox)
         frmbox.addLayout(rightbox)
         self.show()
+
+    def save_img(self):
+        img = self.view.grab(self.view.sceneRect().toRect())
+        # print(img)
+        fname = QFileDialog.getSaveFileName(self, "이따가 저장할래?", "./")
+        # print(fname)
+        if fname[0]:
+            img.save(fname[0])
+        # img.save('e:/data.png')
+
+    def selectColorDlg(self):
+        # 색상 대화상자 생성
+        color = QColorDialog.getColor()
+        this = self.sender()
+        if this == self.penbtn:
+            self.pencolor = color
+        elif this == self.brushbtn:
+            self.brushcolor = color
+
+        this.setStyleSheet("background-color:{}".format(color.name()))
+    
+    # 어느 라디오 버튼이 선택되었는지 판단
+    def radioBtnClicked(self):
+        if self.rbtnLine.isChecked():
+            self.drawType = 1
+        elif self.rbtnRect.isChecked():
+            self.drawType = 2
+        elif self.rbtnCurve.isChecked():
+            self.drawType = 3
+        elif self.rbtnEllipse.isChecked():
+            self.drawType = 4
+    
+
+
 
 # QGraphics 는 시각적 객체의 복잡한 장면을 쉽게 처리할 수 있는 
 # 프레임워크로 구성하는데 사용할 수 있다.
@@ -112,18 +173,53 @@ class CGView(QGraphicsView):
     def mouseMoveEvent(self, e):
         # print("마우스 드래그 됨")
         self.end = e.pos()
+        pen = QPen(self.parent().pencolor, self.parent().combo.currentIndex()+1)
+        brush = QBrush(self.parent().brushcolor)
 
-        pen = QPen(QColor(255,0,0), 1)
-        # 현재선을 추가
-        # line = QLineF(시작x, y, 끝x, y)
-        line = QLineF(self.start.x(), self.start.y(), self.end.x(), self.end.y())
-        # self.scene.addLine(라인객체, 행종류)
-        self.scene.addLine(line, pen)
+        # scene에 그려진 이전 선을 제거
+        if len(self.items) > 0 :
+            self.scene.removeItem(self.items[-1])
+            del(self.items[-1])
+
+        if self.parent().drawType == 1:
+            # 현재선을 추가
+            line = QLineF(self.start.x(), self.start.y(), self.end.x(), self.end.y())
+            self.items.append(self.scene.addLine(line, pen))
+        elif self.parent().drawType == 2:    
+            # 사각형 그리기
+            rect = QRectF(self.start, self.end)
+            self.items.append(self.scene.addRect(rect, pen, brush))
+        elif self.parent().drawType == 3:    
+            # 곡선그리기
+            line = QLineF(self.start.x(), self.start.y(), self.end.x(), self.end.y())
+            self.scene.addLine(line, pen)
+            # 끝점을 다시 시작점으로
+            self.start = e.pos()
+        elif self.parent().drawType == 4:
+            rect = QRectF(self.start, self.end)
+            self.items.append(self.scene.addEllipse(rect, pen, brush))
+            
+
+        # print(self.items)
 
     def mouseReleaseEvent(self, e):
         # print("마우스 뗄 때 호출됨")
-        pass
+        self.end = e.pos()
+        pen = QPen(self.parent().pencolor, self.parent().combo.currentIndex()+1)
+        brush = QBrush(self.parent().brushcolor)
 
+        if self.parent().drawType == 1:
+            line = QLineF(self.start.x(), self.start.y(), self.end.x(), self.end.y())
+            # self.scene.addLine(라인객체, 행종류)
+            self.scene.addLine(line, pen)
+
+        elif self.parent().drawType == 2:    
+            rect = QRectF(self.start, self.end)
+            self.scene.addRect(rect, pen, brush)
+
+        elif self.parent().drawType == 4:
+            rect = QRectF(self.start, self.end)
+            self.scene.addEllipse(rect, pen, brush)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
